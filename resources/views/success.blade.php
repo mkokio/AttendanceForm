950af3b97baa6d935f865ba3a389df5382879808
Marc Cocchio <marccocchio@gmail.com>
2023-10-23 16:26:39 +0900

    Success Screen QR Code

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,16 +1,20 @@
 <x-app-layout>
     <div class="py-12">
         <div class="container">
-            <div class="p-5 bg-white border rounded">
                                 
-                <header>
+                
+                    
                     <h2 class="h4 font-weight-bold text-black">
                         {{ __('Event Created') }}
                     </h2>
-            
+                    <br />
                     <p class="mt-1 text-sm text-gray-600">
-                    <a href="{{ env('GOOGLE_CALENDAR_PUBLIC_URL') }}" class="custom-link" target="_blank">{{ __('You added your event on the Google Calendar.') }}</a>
+                        <a href="{{ env('GOOGLE_CALENDAR_PUBLIC_URL') }}" class="custom-link" target="_blank">{{ __('You added your event on the Google Calendar.') }}</a>
                     </p>
+                    
+                    <div class="d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('calendarQRcode.png') }}" alt="Calendar QR Code">
+                    </div>
                     <br />
                     <hr />
                     <br />
@@ -18,9 +22,8 @@
                         <a href='/attendanceforms' class="custom-link">{{ __('Would you like to create another event?') }}</a>
                     </p>
 
-                </header>
 
-            </div>
+            
         </div>
     </div>
 </x-app-layout>
