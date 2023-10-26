73a2b143f4ed51facea86c63b01e2e1087f69e79
Marc Cocchio <marccocchio@gmail.com>
2023-10-26 15:33:25 +0900

    Added border to QR code on success screen

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -11,9 +11,9 @@
                     <p class="mt-1 text-sm text-gray-600">
                         <a href="{{ env('GOOGLE_CALENDAR_PUBLIC_URL') }}" class="custom-link" target="_blank">{{ __('You added your event on the Google Calendar.') }}</a>
                     </p>
-                    
+                    <br />
                     <div class="d-flex justify-content-center align-items-center">
-                        <img src="{{ asset('calendarQRcode.png') }}" alt="Calendar QR Code">
+                        <img clasS="border border-warning rounded" src="{{ asset('calendarQRcode.png') }}" alt="Calendar QR Code">
                     </div>
                     <br />
                     <hr />
