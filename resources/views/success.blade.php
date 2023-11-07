1d4c6d0df4fe4631f6d991e20af6feca2f2334a4
Marc Cocchio <marccocchio@gmail.com>
2023-11-07 13:04:17 +0900

    Embedded calendar in success page

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,9 +1,6 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="container">
-                                
-                
-                    
+        <div class="container">    
                     <h2 class="h4 font-weight-bold text-black">
                         {{ __('Event Created') }}
                     </h2>
@@ -13,16 +10,18 @@
                     </p>
                     <br />
                     <div class="d-flex justify-content-center align-items-center">
-                        <img clasS="border border-warning rounded" src="{{ asset('calendarQRcode.png') }}" alt="Calendar QR Code">
+                        <img class="border border-warning rounded" src="{{ asset('calendarQRcode.png') }}" alt="Calendar QR Code" style="width: 50%;" >
                     </div>
+                    <br />
+                    <p class="mt-1 text-sm">
+                        <a href='/attendanceforms' class="link-info">{{ __('Would you like to create another event?') }}</a>
+                    </p>
                     <br />
                     <hr />
                     <br />
-                    <p class="mt-1 text-sm text-gray-600">
-                        <a href='/attendanceforms' class="link-info">{{ __('Would you like to create another event?') }}</a>
-                    </p>
-
-
+                    <div class="d-flex justify-content-center">
+                        <iframe src="{{ env('GOOGLE_CALENDAR_PUBLIC_URL') }}" style="border-width:0" width="600" height="600" frameborder="0" scrolling="no"></iframe>
+                    </div>
             
         </div>
     </div>
