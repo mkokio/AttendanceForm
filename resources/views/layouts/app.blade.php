7ce69e39526fc1c16a3dfc7bf8cf9615c542e5e5
Marc Cocchio <marccocchio@gmail.com>
2023-11-18 18:18:37 +0900

    Minor adjustments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,7 @@
             {{ $slot }}
         </main>
     </div>
+<br>
     
     <!-- Bootstrap JavaScript and Popper.js from CDN -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
